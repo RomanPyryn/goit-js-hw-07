c447080df2a399da4d697934088b0f0f75ac71f9
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-04-27 15:26:31 +0300

    Додає друге завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createsGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createsGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" />
+        </a>
+        `
+    } ).join('');
+};
+
+galleryEl.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+    e.preventDefault();
+
+    if (e.target.classList.value !== 'gallery__image') {
+        return;
+    }
+
+    console.log(e.target.classList.value);
+};
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
