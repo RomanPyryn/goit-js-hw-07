5b68b587864e552d6600aa9a6d4f710a69fc7085
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-04-27 13:31:14 +0300

    Додає перше завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createsGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+function createsGalleryItemsMarkup(galleryItems) {
+    return  galleryItems.map(({ preview, original, description }) => {
+      return  `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"   
+            />
+        </a>
+    </div>
+    `;
+    }).join('');
+}
+
+galleryEl.addEventListener('click', onItemClick);
+function onItemClick(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') { return; };
+
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}">
+    `)
+    instance.show()
+
+
+    window.addEventListener('keydown', onModalClose);
+    function onModalClose(e) {
+        if (e.key !== 'Escape') { return; };
+        
+        instance.close();
+    };
+};
+
